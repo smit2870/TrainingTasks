26b74066c85145af7a366604849a65145e0eda60
smit2870 <smit.kachhadiya@zeuslearning.com>
2026-06-25 12:38:50 +0000

    refactor: Remove weatherforecast endpoint and cleanup

--- TrainingDirectory.Api/Program.cs
+++ TrainingDirectory.Api/Program.cs
@@ -6,19 +6,6 @@ using TrainingDirectory.Api.Clients;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Env.Load();
-
-// var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-// var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
-// var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-// var dbUser = Environment.GetEnvironmentVariable("DB_USER");
-// var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
-
-// var connectionString = $"server={dbHost};port={dbPort};database={dbName};user={dbUser};password={dbPassword};";
-
-// builder.Services.AddDbContext<AppDbContext>(options =>
-//     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
@@ -76,7 +63,6 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -86,28 +72,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
